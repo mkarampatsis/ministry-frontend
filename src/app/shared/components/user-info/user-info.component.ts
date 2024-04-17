4fb267caeac74fd3089c1ba491e985026c2c1c2d
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2024-04-17 12:41:02 +0300

    user-info dropdown menu

--- src/app/shared/components/user-info/user-info.component.ts
+++ src/app/shared/components/user-info/user-info.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Component({
     selector: 'app-user-info',
     standalone: true,
-    imports: [CommonModule, MatIconModule],
+    imports: [CommonModule, MatIconModule, NgbDropdownModule],
     templateUrl: './user-info.component.html',
     styleUrl: './user-info.component.css',
 })
