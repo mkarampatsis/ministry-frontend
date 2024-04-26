a877a348adb0d0f50514996f2a1cde2602b9f56d
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2024-04-26 11:54:50 +0300

    css hacks

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,13 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { AuthService } from 'src/app/shared/services/auth.service';
-import { UserNavbarComponent } from '../user-navbar/user-navbar.component';
 import { UserInfoComponent } from '../../shared/components/user-info/user-info.component';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
     selector: 'app-navigation',
     standalone: true,
-    imports: [CommonModule, UserNavbarComponent, UserInfoComponent],
+    imports: [RouterLink, RouterLinkActive, UserInfoComponent],
     templateUrl: './navigation.component.html',
     styleUrl: './navigation.component.css',
 })
--- src/app/components/user-navbar/user-navbar.component.ts
+++ src/app/components/user-navbar/user-navbar.component.ts
@@ -1,11 +1,10 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
     selector: 'app-user-navbar',
     standalone: true,
-    imports: [CommonModule, RouterLink, RouterLinkActive],
+    imports: [RouterLink, RouterLinkActive],
     templateUrl: './user-navbar.component.html',
     styleUrl: './user-navbar.component.css',
 })
