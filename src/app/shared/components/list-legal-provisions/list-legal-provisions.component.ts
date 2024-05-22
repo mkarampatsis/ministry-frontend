b61f90fee4a31eddb142684da787612c268ee7d9
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2024-05-22 17:19:14 +0300

    legal provision sorting

--- src/app/shared/components/list-legal-provisions/list-legal-provisions.component.ts
+++ src/app/shared/components/list-legal-provisions/list-legal-provisions.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, inject } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges, inject } from '@angular/core';
 import { ClipboardModule } from '@angular/cdk/clipboard';
 
 import { ILegalProvision } from '../../interfaces/legal-provision/legal-provision.interface';
@@ -12,13 +12,40 @@ import { LegalProvisionService } from '../../services/legal-provision.service';
     templateUrl: './list-legal-provisions.component.html',
     styleUrl: './list-legal-provisions.component.css',
 })
-export class ListLegalProvisionsComponent {
+export class ListLegalProvisionsComponent implements OnChanges {
     @Input() legalProvisions: ILegalProvision[] = [];
     @Input() code = '';
     @Input() provisionType: 'organization' | 'ogranizationalUnit' | 'remit' | null = null;
     @Input() actionColumnVisible = false;
     modalService = inject(ModalService);
     legalProvisionService = inject(LegalProvisionService);
+
+    ngOnChanges(changes: SimpleChanges): void {
+        if (changes.legalProvisions) {
+            this.legalProvisions = changes.legalProvisions.currentValue;
+            this.sortLegalProvisions();
+        }
+    }
+
+    sortLegalProvisions() {
+        const legalProvisions = this.legalProvisions;
+
+        const sortedData = legalProvisions.map((obj) => {
+            const dateStr = obj.legalActKey.match(/\d{2}-\d{2}-\d{4}$/)?.[0]; // Extract date in format DD-MM-YYYY
+            if (dateStr) {
+                const [day, month, year] = dateStr.split('-');
+                const isoDateStr = `${year}-${month}-${day}`;
+                const date = new Date(isoDateStr);
+                return { ...obj, date };
+            } else {
+                return { ...obj, date: null };
+            }
+        });
+
+        // Sort the array by date in descending order
+        sortedData.sort((a, b) => (b.date?.getTime() || 0) - (a.date?.getTime() || 0));
+        this.legalProvisions = sortedData;
+    }
 
     displayLegalProvision(provision: ILegalProvision) {
         this.modalService.showLegalProvision(provision);
